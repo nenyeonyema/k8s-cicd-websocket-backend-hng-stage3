c9168069048032b01306104ece11f760b12c5b10
nenyeonyema <chinenyeonyema1@gmail.com>
2025-02-21 16:43:46 +0000

    modified the application file

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
 def main():
-    return "Hello, World!"  
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    print(main())
+    app.run(host="0.0.0.0", port=5000)
+
