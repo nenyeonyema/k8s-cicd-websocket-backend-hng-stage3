be41aa5a61fc7191dcd5cd4a0f11e0fb2f1a9757
nenyeonyema <chinenyeonyema1@gmail.com>
2025-02-25 22:45:49 +0000

    modified my app entry and test file

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello World"
+    return "Jenkins CICD deployed on Kubernetes by Chinenye Genevieve Onyema"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- test_helloworld.py
+++ test_helloworld.py
@@ -3,7 +3,7 @@ from helloworld import main
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(main(), "Hello World")
+        self.assertEqual(main(), "Jenkins CICD deployed on Kubernetes by Chinenye Genevieve Onyema")
 
 if __name__ == "__main__":
     unittest.main()
